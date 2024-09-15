4b1265caf7e7cb1835fec7898b761693f851a4fd
Tony Lea <tnylea@gmail.com>
2024-09-15 09:18:44 -0400

    Adding original way of adding dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: 'selector',
+    darkMode: 'class',
     content: [
         "./**/*.{html,js,md}",
         "./*.{html,js,md}"
